3e937e7832b3e0759b2094641a97465a81cf396e
savindug <savibashnon11@gmail.com>
2020-02-12 14:52:56 +0530

    commmit 1

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+{{--                <button id = "div12" class="btn btn-info" style="display:block" onclick = "replace('div1','div2')">Events</button>--}}
+{{--                <button id = "div22" class="btn btn-success" style="display:block" onclick = "replace('div2','div1')">Posts</button>--}}
+                <ul class="nav navbar-nav">
+
+                    <li><a  onclick = "replace('div1','div2')">Events</a></li>
+                    <li><a  onclick = "replace('div1','div2')">Posts</a></li>
+
+                </ul>
+
+
+
+                <div class="panel-body">
+                    <div id="div1" style="display:block">
+                        <a href="/posts/create" class="btn btn-primary">Create Post</a>
+                        <h3>Your Blog Posts</h3>
+                        @if(count($posts) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach($posts as $post)
+                                    <tr>
+                                        <td>{{$post->title}}</td>
+                                        <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                        <td>
+                                            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>You have no posts</p>
+                        @endif
+                    </div>
+
+                    <div id="div2" style="display:none">
+                        <a href="/posts/create" class="btn btn-primary">Create Event</a>
+                        <h3>Your Events</h3>
+                        @if(count($posts) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach($posts as $post)
+                                    <tr>
+                                        <td>{{$post->title}}</td>
+                                        <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                        <td>
+                                            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>You have no posts</p>
+                        @endif
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function replace( hide, show ) {
+        document.getElementById(hide).style.display="none";
+        document.getElementById(show).style.display="block";
+    }
+</script>
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="/posts" class="btn btn-default">Go Back</a>
+    <h1>{{$post->title}}</h1>
+    <img style="width:100%" src="/storage/app/public/cover_images/add_reports_1581428995.png">
+    <img src="/public/cover_images/add_reports_1581428995.png" class="img-fluid" alt="Responsive image">
+    <h3>{!!$post->cover_image!!}</h3>
+    <br><br>
+    <div>
+        {!!$post->body!!}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+    <hr>
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+        @endif
+    @endif
+@endsection
